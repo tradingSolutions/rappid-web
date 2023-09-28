f0b748f962bc15c6708fbf4654312b710425b0d8
tradingSolutions <trading.solutions.mg23@gmail.com>
2023-09-28 13:25:46 -0300

    SEO

---
+++ send.php
@@ -0,0 +1,35 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nombre = strip_tags(trim($_POST["nombre"]));
+    $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+    $mensaje = trim($_POST["mensaje"]);
+
+    // Verifica que los datos no estén vacíos y que el correo electrónico sea válido
+    if (empty($nombre) OR empty($mensaje) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        header("Location: http://tudominio.com?error");
+        exit;
+    }
+
+    // Dirección de correo electrónico a la que deseas que se envíe el formulario
+    $recipient = "tucorreo@tudominio.com";
+
+    // Asunto del correo electrónico
+    $subject = "Nuevo mensaje de $nombre";
+
+    // Contenido del correo electrónico
+    $email_content = "Nombre: $nombre\n";
+    $email_content .= "Correo Electrónico: $email\n\n";
+    $email_content .= "Mensaje:\n$mensaje\n";
+
+    // Encabezados de correo electrónico
+    $email_headers = "From: $nombre <$email>";
+
+    // Enviar el correo electrónico
+    if (mail($recipient, $subject, $email_content, $email_headers)) {
+        header("Location: http://tudominio.com?success");
+    } else {
+        header("Location: http://tudominio.com?error");
+    }
+} else {
+    header("Location: http://tudominio.com");
+}
