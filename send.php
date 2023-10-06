5955d3e01d848e00198b037d21c5eb29c47ce558
tradingSolutions <trading.solutions.mg23@gmail.com>
2023-10-06 13:13:27 -0300

    landing

--- send.php
+++ send.php
@@ -6,12 +6,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // Verifica que los datos no estén vacíos y que el correo electrónico sea válido
     if (empty($nombre) OR empty($mensaje) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        header("Location: http://tudominio.com?error");
+        header("Location: http://rappidtech.com?error");
         exit;
     }
 
     // Dirección de correo electrónico a la que deseas que se envíe el formulario
-    $recipient = "tucorreo@tudominio.com";
+    $recipient = "nosotros@rappidtech.com";
 
     // Asunto del correo electrónico
     $subject = "Nuevo mensaje de $nombre";
@@ -26,10 +26,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // Enviar el correo electrónico
     if (mail($recipient, $subject, $email_content, $email_headers)) {
-        header("Location: http://tudominio.com?success");
+        header("Location: http://rappidtech.com?success");
     } else {
-        header("Location: http://tudominio.com?error");
+        header("Location: http://rappidtech.com?error");
     }
 } else {
-    header("Location: http://tudominio.com");
+    header("Location: http://rappidtech.com");
 }
